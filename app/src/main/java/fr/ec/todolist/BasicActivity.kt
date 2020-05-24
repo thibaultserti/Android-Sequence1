961c0303e0fd13e392b59d92482ad4e9de5f9d96
Thibault Serti <thibault.serti@gmail.com>
2020-05-24 20:02:21 +0200

    Add menu

--- app/src/main/java/fr/ec/todolist/BasicActivity.kt
+++ app/src/main/java/fr/ec/todolist/BasicActivity.kt
@@ -2,11 +2,42 @@ package fr.ec.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 
 abstract class BasicActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basic)
+    }
+
+    fun setupToolBar() {
+        val toolbar: Toolbar= findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.settings->{
+                //Vous exécutez votre code ici
+                true
+            }
+            R.id.apropos->{
+                //Vous exécutez votre code ici
+                true
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/fr/ec/todolist/MainActivity.kt
+++ app/src/main/java/fr/ec/todolist/MainActivity.kt
@@ -1,11 +1,14 @@
 package fr.ec.todolist
 
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : BasicActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.setupToolBar()
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(false)
     }
 }

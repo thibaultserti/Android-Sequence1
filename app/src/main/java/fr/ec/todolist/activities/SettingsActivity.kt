21c688e5cc53aea7ae9d560ff7c623f95b40c1b0
MashedP <cyrousselot@gmail.com>
2020-05-31 11:07:39 +0200

    Ajout d'un recycler view pour les settings, il faut que je le modifie car il marche pas encore très bien Ajout d'un adapter pour le recycler view,  à modifier

--- app/src/main/java/fr/ec/todolist/activities/SettingsActivity.kt
+++ app/src/main/java/fr/ec/todolist/activities/SettingsActivity.kt
@@ -1,15 +1,37 @@
 package fr.ec.todolist.activities
 
+import android.app.Activity
 import android.os.Bundle
 import android.widget.FrameLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import fr.ec.todolist.R
+import fr.ec.todolist.adapters.LanguageAdapter
 
 class SettingsActivity : BasicActivity() {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         super.setupToolBar()
         val activityContainer: FrameLayout = findViewById(R.id.layout_container)
+        setContentView(R.layout.activity_settings)
         layoutInflater.inflate(R.layout.activity_settings, activityContainer, true)
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = LanguageAdapter()
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerView2).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+
+
+        }
     }
 }
+
